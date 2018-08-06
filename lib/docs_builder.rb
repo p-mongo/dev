378de80941afbccbf4d80903a620118bb0e6725a
Oleg Pudeyev <oleg@bsdpower.com>
2018-08-06 18:36:43 -0400

    Docs building script

---
+++ lib/docs_builder.rb
@@ -0,0 +1,104 @@
+require 'yaml'
+require 'childprocess'
+require 'active_support/core_ext/class/attribute'
+require 'fileutils'
+require 'pathname'
+
+class DocsBuilder
+  class_attribute :work_path
+  class_attribute :docs_repo_url
+  class_attribute :build_root
+
+  self.build_root = Pathname.new(File.expand_path('~/tmp/docs-build'))
+
+  def setup
+    FileUtils.mkdir_p(self.class.build_root)
+  end
+
+  def build
+    setup
+
+    bn = self.class.work_path.basename
+
+    dr_work_path = self.class.build_root.join(bn)
+    if File.exist?(dr_work_path)
+      check_call(['sh', '-c', <<S], cwd: dr_work_path)
+        git checkout master &&
+        git fetch origin &&
+        git reset --hard origin/master
+S
+    else
+      check_call(%w(git clone) + [self.class.docs_repo_url, dr_work_path])
+    end
+
+    cr_work_path = self.class.build_root.join('repo')
+    if File.exist?(cr_work_path)
+      check_call(['sh', '-c', <<S], cwd: cr_work_path)
+        git checkout master &&
+        git fetch origin &&
+        git reset --hard origin/master
+S
+    else
+      check_call(%w(git clone) + [self.class.work_path, cr_work_path])
+    end
+
+    check_call(['sh', '-c', <<S], cwd: cr_work_path)
+        git checkout build || git checkout -b build &&
+        git reset --hard master
+S
+    check_call(['echo', 'rsync', '-av', '--delete', '--exclude', '.git',
+      self.class.work_path.to_s + '/', cr_work_path])
+    check_call(['sh', '-c', <<S], cwd: cr_work_path)
+        git add . &&
+        if ! git status |grep -q 'nothing to commit'; then
+          git commit -amBuild
+        fi
+S
+
+    contents = File.read(dr_work_path.join('config', 'build_conf.yaml'))
+    config = YAML.load(contents)
+    config['assets'].each do |asset|
+      if asset['repository'] =~ /ruby-driver/
+        asset['repository'] = cr_work_path.to_s
+        asset['branch'] = 'build'
+        break
+      end
+    end
+
+    File.open(dr_work_path.join('config', 'build_conf.yaml'), 'w') do |f|
+      f << YAML.dump(config)
+    end
+
+    check_call(['sh', '-c', <<S], cwd: dr_work_path)
+        make clean html
+S
+  end
+
+  def check_call(cmd, env: nil, cwd: nil)
+    process = ChildProcess.new(*cmd.map { |p| p.to_s })
+    process.io.inherit!
+    if env
+      env.each do |k, v|
+        process.environment[k.to_s] = v
+      end
+    end
+    if cwd
+      process.cwd = cwd
+    end
+    process.start
+    process.wait
+    unless process.exit_code == 0
+      raise "Failed to execute: #{cmd}"
+    end
+  end
+end
+
+class DriverDocsBuilder < DocsBuilder
+  self.work_path = Pathname.new(File.expand_path('~/apps/ruby-driver'))
+  self.docs_repo_url = 'https://github.com/mongodb/docs-ruby'
+end
+
+class MongoidDocsBuilder < DocsBuilder
+  self.work_path = Pathname.new(File.expand_path('~/apps/mongoid'))
+  self.docs_repo_url = 'https://github.com/mongodb/docs-mongoid'
+end
